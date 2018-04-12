64f9a21b792001729be51ae604120e2309c3aea7
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-04-12 17:20:16 +0200

    add google maps api

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-    this.firstName = "Jack";
-    this.lastName = "Middlebrook";
+    this.firstName = ko.observable("Jack");
+    this.lastName = ko.observable("Middlebrook");
+    this.fullName = ko.computed(
+        function () {
+            return this.firstName() + " " + this.lastName();
+        }, this
+    );
 }
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
+
+// use google maps api to display map of Amsterdam
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 52.3702, lng: 4.8952},
+        zoom: 8
+    });
+}
