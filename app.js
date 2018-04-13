55e846c2b8a31f4af72e9b16d1ac3464d0f8c185
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-04-13 12:48:24 +0200

    add overflow scroll

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+// variable for google maps api to display map of Amsterdam
+var map;
+// Create a new blank array for all the listing markers.
+var markers = [];
+// List of museums
+var museums = [];
+
 function AppViewModel() {
-    this.firstName = ko.observable("Jack");
-    this.lastName = ko.observable("Middlebrook");
-    this.fullName = ko.computed(
-        function () {
-            return this.firstName() + " " + this.lastName();
-        }, this
-    );
+    this.searchMuseum = ko.observable("");
+
     this.museums = ko.observableArray([
         {title: 'Rijksmuseum', location: {lat: 52.3600, lng: 4.8852}},
         {title: 'Van Gogh Museum', location: {lat: 52.3584, lng: 4.8811}},
@@ -17,17 +18,16 @@ function AppViewModel() {
         {title: 'Museum het Rembrandthuis', location: {lat: 52.3694, lng: 4.9012}},
         {title: 'Allard Pierson Museum', location: {lat: 52.3687, lng: 4.8930}},
         {title: 'Tropenmuseum', location: {lat: 52.3627, lng: 4.9223}}
-    ])
+    ]);
+
+    // initialize the array of museums for markers
+    museums = this.museums();
+
 
 }
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
-
-// use google maps api to display map of Amsterdam
-var map;
-// Create a new blank array for all the listing markers.
-var markers = [];
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -35,29 +35,13 @@ function initMap() {
         zoom: 13
     });
 
-    // list of amsterdam museums with coordinates
-    var museums = [
-        {title: 'Rijksmuseum', location: {lat: 52.3600, lng: 4.8852}},
-        {title: 'Van Gogh Museum', location: {lat: 52.3584, lng: 4.8811}},
-        {title: 'Joods Historisch Museum', location: {lat: 52.3671, lng: 4.9038}},
-        {title: 'Fotografiemuseum Amsterdam', location: {lat: 52.3641, lng: 4.8933}},
-        {title: 'Stedelijk Museum', location: {lat: 52.3580, lng: 4.8798}},
-        {title: 'Hermitage Museum', location: {lat: 52.3653, lng: 4.9024}},
-        {title: 'Museum het Rembrandthuis', location: {lat: 52.3694, lng: 4.9012}},
-        {title: 'Allard Pierson Museum', location: {lat: 52.3687, lng: 4.8930}},
-        {title: 'Tropenmuseum', location: {lat: 52.3627, lng: 4.9223}}
-    ];
-
+    // for updating the info window
     var largeInfowindow = new google.maps.InfoWindow();
-
-    // default marker icon for museums
+    // Marker icon for museums
     var defaultIcon = makeMarkerIcon('0091ff');
-
     var highlightedIcon = makeMarkerIcon('ffff24');
 
-
-
-    // The following group uses the museum array to create an array of markers on initialize.
+    // create markers for each location
     for (var i = 0; i < museums.length; i++) {
         // Get the position from the location array.
         var position = museums[i].location;
@@ -89,13 +73,11 @@ function initMap() {
                 largeInfowindow.close();
             }
         });
-
-
     }
 
     document.getElementById('show-museums').addEventListener('click', showMuseums);
     document.getElementById('hide-museums').addEventListener('click', hideMuseums);
-
+    document.getElementById('search-museums').addEventListener('click', searchMuseums)
 }
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
@@ -129,6 +111,10 @@ function hideMuseums() {
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
     }
+}
+
+function searchMuseums() {
+    console.log("searching for...");
 }
 
 function makeMarkerIcon(markerColor) {
